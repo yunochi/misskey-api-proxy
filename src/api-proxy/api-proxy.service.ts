0713a887587c51ce8501602e02abc24a2f4cfab3
Yuno <yuno@yunochi.com>
2024-08-14 00:23:20 +0900

    access token 자동삭제 수정

--- src/api-proxy/api-proxy.service.ts
+++ src/api-proxy/api-proxy.service.ts
@@ -35,28 +35,32 @@ export class ApiProxyService {
           }),
         ),
     );
-    this.logger.log(`user ${data?.username} login`);
-    if (this.autoRevokeToken === 'yes') {
-      const token = auth?.slice(7);
-      this.logger.log(`delete token: ${token}...`);
-      await firstValueFrom(
-        this.httpService
-          .post(
-            `${this.misskey_url}/api/i/revoke-token`,
-            { token: token },
-            { headers: { Authorization: auth, 'x-forwarded-for': remote_ip } },
-          )
-          .pipe(
-            catchError((error: AxiosError) => {
-              this.logger.error(error.response?.data);
-              throw new HttpException(
-                error.response?.data ?? 'Error',
-                error.status ?? 500,
-              );
-            }),
-          ),
-      );
-      this.logger.log(`token ${token} deleted`);
+    this.logger.log(`user ${data?.username} login OK`);
+
+    if (this.autoRevokeToken === '1') {
+      const token = auth?.split(' ')[1];
+      this.logger.log(`delete access token: ${token}...`);
+      try {
+        await firstValueFrom(
+          this.httpService
+            .post(
+              `${this.misskey_url}/api/i/revoke-token`,
+              { token: token },
+              {
+                headers: { Authorization: auth, 'x-forwarded-for': remote_ip },
+              },
+            )
+            .pipe(
+              catchError((error: AxiosError) => {
+                this.logger.warn(error.response?.data);
+                throw new Error(`Error!`);
+              }),
+            ),
+        );
+        this.logger.log(`Access token ${token} deleted`);
+      } catch (err) {
+        this.logger.warn(`Access token ${token} delete error`);
+      }
     }
     return data;
   }
